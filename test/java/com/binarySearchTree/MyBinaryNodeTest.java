42317bcf01b5c26ca40b8fa89c6c092ff056ead0
renu100 <renu.chandraker08@gmail.com>
2021-10-07 17:32:59 +0530

    Create Tree

---
+++ test/java/com/binarySearchTree/MyBinaryNodeTest.java
@@ -0,0 +1,17 @@
+package com.binarySearchTree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+class MyBinaryNodeTest {
+
+	@Test
+	public void given3NumbersWhenAddedToBSTShouldReturnSize3() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(3, size);
+	}
+}
