ed06944d6a17586a55c4ad30a100d044910464c2
renu100 <renu.chandraker08@gmail.com>
2021-10-08 19:41:21 +0530

    Search 63 in the binary tree

--- test/java/com/binarySearchTree/MyBinaryNodeTest.java
+++ test/java/com/binarySearchTree/MyBinaryNodeTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 public class MyBinaryNodeTest {
 
-
 	@Test
 	public void given3NumbersWhenAddedToBSTShouldReturnSize3() {
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
@@ -36,5 +35,28 @@ public class MyBinaryNodeTest {
 		myBinaryTree.add(67);
 		int size = myBinaryTree.getSize();
 		Assert.assertEquals(13, size);
+	}
+
+	@Test
+	public void givenNumbersWhenSearchToBSTShouldReturn63() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+
+		myBinaryTree.add(16);
+		myBinaryTree.add(3);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		boolean result = myBinaryTree.containsNodeRecursive(myBinaryTree.root, 63);
+		Assert.assertEquals(true, result);
 	}
 }
